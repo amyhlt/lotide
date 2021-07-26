074b345bb58e00be9939a6c0e831b5d2475324be
AmyTang <huilitang1@gmail.com>
2021-07-26 08:30:16 -0700

    Change asserEqual

--- assertEqual.js
+++ assertEqual.js
@@ -3,16 +3,13 @@ const assertEqual = function(actual, expected) {
 
   if (actual === expected) {
 
-    console.log('😃😃😃' + "Assertion Passed:" + " " + actual + " ===" + expected); 
-    
+    console.log(`😃😃😃Assertion Passed: ${actual}  === ${expected}`); 
+
   }
   else {
 
-    console.log('😩😩😩' + "Assertion Failed:" + " " + actual + " !==" + expected);
+    console.log(`😩😩😩Assertion Failed:  ${actual} !==${expected}`);
     
   }
 };
-
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+module.exports = assertEqual;
