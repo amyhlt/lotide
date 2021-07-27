3f35523d81e5ce942032787416ee6065d8287d13
AmyTang <huilitang1@gmail.com>
2021-07-26 22:04:41 -0700

    update files

--- countOnly.js
+++ countOnly.js
@@ -1,44 +1,23 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`😃😃😃Assertion Passed: ${actual}  === ${expected}`);
-  } else {
-    console.log(`😩😩😩Assertion Failed:  ${actual} !==${expected}`);
-  }
-};
+
 let obj={};
 let count=0;
 const countOnly = function(allItems, itemsToCount) {
     
     
     for (let key in itemsToCount) {
-        if (itemsToCount[key] === true) {
+      if (itemsToCount[key] === true) {
             for (let a of allItems) {
                 if (key === a) {
                     count++;
                 }
             }
             if (count !== 0)
-              obj[key]=count;
+              obj[key] = count;
+            else
+            obj[key] = 0;
         }
-        count=0;
-    }
-    return obj;
+        count = 0;
+     }
+     return obj;
 }
-const firstNames = [
-    "Karl",
-    "Salima",
-    "Agouhanna",
-    "Fang",
-    "Kavith",
-    "Jason",
-    "Salima",
-    "Fang",
-    "Joe"
-  ];
-  
-  const result1 = countOnly(firstNames, { "Jason": true,"Amy":true, "Karima": true, "Fang": true, "Agouhanna": false });
-  console.log(obj);
-  assertEqual(result1["Jason"], 1);
-  assertEqual(result1["Karima"], 0);
-  assertEqual(result1["Fang"], 2);
-  assertEqual(result1["Agouhanna"],0);
+module.exports = countOnly;
--- without.js
+++ without.js
@@ -1,41 +1,11 @@
-function eqArrays(arr1,arr2) {
-  let num = 0;
-  if (arr1.length === arr2.length) {
-    for (let i = 0; i < arr1.length; i ++) {
-      if (arr1[i] === arr2[i]) {
-        num ++;
-      }
-    }
-    if (num === arr1.length) {
-      return true;
-    }
-    else
-      return false;
-  }
-  else 
-    return false;
-}
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`😃😃😃Assertion Passed: ${actual}  === ${expected}`); 
-  }
-  else {
-    console.log(`😩😩😩Assertion Failed:  ${actual} !==${expected}`);
-  }
-};
-function assertArraysEqual(arr1,arr2) {
-  return assertEqual(eqArrays(arr1,arr2),true);
-}
+
 function without(original,removal) {
   let a = [];
   for (let n of original)
     if (!(removal.includes(n))){
       a.push(n);
     }
-  console.log(a);
+  return a;
 }
-const words = ["hello", "world", "lighthouse"];
-without(words, ["lighthouse"]); 
-without([1, 2, 3], [1]);
-without(["1", "2", "3"], [1, 2, "3"]);
-assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+console.log(without([1,2,3], [1]));
+module.exports = without;
